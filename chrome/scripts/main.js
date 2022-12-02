161345b591829636ec6342fcaa3b749b6351eaa5
Danella Olsen <danella.olsen@gmail.com>
2022-12-02 10:59:50 -0700

    :sparkles: Adding a mutationObserver to watch for DOM changes

--- chrome/scripts/main.js
+++ chrome/scripts/main.js
@@ -1,8 +1,44 @@
-const elements = document.querySelectorAll("input", "form")
 
-console.log(`Discovered ${elements.length} that could potentially autocomplete.`)
+const AUTOFILLABLE_NODE_TYPES = [
+  'input',
+  'form'
+]
 
-elements.forEach((element) => {
+const OBSERVER_CONFIG = Object.freeze({
+  childList: true,
+  subtree: true
+});
+
+onPageLoad()
+
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    const { addedNodes } = mutation
+
+    addedNodes.forEach(traverseNodes)
+  });
+});
+
+// Observe the body (and its descendants) for `childList` changes.
+observer.observe(document.body, OBSERVER_CONFIG);
+
+function traverseNodes(node) {
+  if(AUTOFILLABLE_NODE_TYPES.includes(node.nodeName.toLowerCase())) disableAutofill(node)
+
+  node.childNodes.forEach((childNode) => traverseNodes(childNode))
+}
+
+function onPageLoad() {
+  const elements = document.querySelectorAll(...AUTOFILLABLE_NODE_TYPES)
+  elements.forEach(disableAutofill)
+}
+
+function disableAutofill(element) {
   console.log("Setting autocomplete=disabled for:", element)
   element.setAttribute('autocomplete', 'disabled')
-})
+}
+
+window.onbeforeunload = function(){
+  console.log("Disconnecting Observer")
+  observer.disconnect()
+}
